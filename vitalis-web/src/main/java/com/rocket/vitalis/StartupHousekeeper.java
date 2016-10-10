e6deade46d998837479831bd3222722158a4f1bd
sescotti <sebastian.scotti@mercadolibre.com>
2016-10-10 19:31:51 -0300

    Init data on dev startup only

--- vitalis-web/src/main/java/com/rocket/vitalis/StartupHousekeeper.java
+++ vitalis-web/src/main/java/com/rocket/vitalis/StartupHousekeeper.java
@@ -3,6 +3,7 @@ package com.rocket.vitalis;
 import com.rocket.vitalis.services.WarmupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
  * Created by sscotti on 10/9/16.
  */
 @Component
+@Profile("dev")
 public class StartupHousekeeper implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
