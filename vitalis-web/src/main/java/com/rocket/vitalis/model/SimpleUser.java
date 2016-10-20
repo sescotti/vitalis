c205be53ec55265c110c9afca464e9e821725a21
ailinmerlo87 <ailin.merlo@gmail.com>
2016-10-19 21:32:15 -0300

    RequestService
    Se pasa todo a Request Service y se crean las clases Simple para cada
    modelo

---
+++ vitalis-web/src/main/java/com/rocket/vitalis/model/SimpleUser.java
@@ -0,0 +1,11 @@
+package com.rocket.vitalis.model;
+
+/**
+ * Created by Sony on 19/10/2016.
+ */
+public interface SimpleUser {
+    Long getId();
+    UserType getUserType();
+    String getName();
+    String getPictureUrl();
+}
