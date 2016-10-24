b93eebd2924146a3e6bd7ff0bab992ef1df6dc6f
ailinmerlo87 <ailin.merlo@gmail.com>
2016-10-24 00:02:44 -0300

    Se actualiza para usar el MeasureService

--- vitalis-web/src/main/java/com/rocket/vitalis/web/controller/api/module/MeasureController.java
+++ vitalis-web/src/main/java/com/rocket/vitalis/web/controller/api/module/MeasureController.java
@@ -1,25 +1,17 @@
 package com.rocket.vitalis.web.controller.api.module;
 
 import com.rocket.vitalis.dto.MeasureRequest;
-import com.rocket.vitalis.repositories.MeasurementRepository;
-import com.rocket.vitalis.repositories.ModuleRepository;
-import com.rocket.vitalis.repositories.MonitoringRepository;
 import com.rocket.vitalis.model.Measurement;
-import com.rocket.vitalis.model.Monitoring;
-import com.rocket.vitalis.model.MeasurementType;
-import com.rocket.vitalis.model.User;
-import com.rocket.vitalis.model.Module;
+import com.rocket.vitalis.services.MeasureService;
 import com.rocket.vitalis.web.controller.api.AbstractApiController;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
-import static com.rocket.vitalis.model.MeasurementType.BLOOD_PRESSURE;
-import static java.time.Instant.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
@@ -29,56 +21,21 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping(value = "/api/measures", consumes = "application/json", produces = "application/json")
 @Log4j
 public class MeasureController  extends AbstractApiController {
+
     @Autowired
-    private MeasurementRepository measurementRepository;
-    @Autowired
-    private MonitoringRepository monitoringRepository;
-    @Autowired
-    private ModuleRepository moduleRepository;
-
-    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
-    @ResponseBody
-    public Object postMeasure(@PathVariable("userId") Long userId, @RequestBody MeasureRequest request){
-        log.info("Received + " + request);
-
-        User user = new User("sebastian@vitalis.com", "sarasapass");
-        user.setId(1000L);
-
-        return user;
-    }
-
+    private MeasureService measureService;
 
     @RequestMapping(method = POST, value = "/measureup", consumes = "application/json", produces = "application/json")
     @ResponseBody
-    public Object measureUp(@RequestBody MeasureRequest request) {
-        /* Get MODULE */
-        Module module = moduleRepository.findOne(request.getIdModule());
-        System.out.println("Modulo encontrado");
-
-        /* Get Last MONITORING  from MODULE */
-        Monitoring monitoring = monitoringRepository.findByModuleAndFinishDateIsNull(module);
-
-
-        /*Get measureType from string */
-        //MeasurementType measurementType = MeasurementType.fromString(request.getMeasureName());
-
-        /* Get Date from string */
-        Date measureDate = null;
-        try {
-            String dateS = request.getMeasureDate();
-            measureDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").parse(dateS);
-        } catch (ParseException e) {
-            measureDate = new Date();
+    public ResponseEntity<?> measureUp(@RequestBody MeasureRequest request) {
+        try{
+            Measurement measurement = measureService.setMeasurement(request.getIdModule(),request.getMeasureDate(), request.getMeasureName(), request.getValue(), request.getValueSecondary());
+            return new ResponseEntity<>(measurement, OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("{\"error\": \"" + e.getMessage() + "\"}", BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("{\"error\": \"internal_server_error\"}", INTERNAL_SERVER_ERROR);
         }
-
-        /* Create new MEASURE */
-        Measurement measure;
-        if (BLOOD_PRESSURE.equals(request.getMeasureName()))
-            measure = new Measurement(monitoring,measureDate,request.getMeasureName(), request.getValue(), request.getValueSecondary());
-        else
-            measure = new Measurement(monitoring,measureDate,request.getMeasureName(), request.getValue());
-
-        return measurementRepository.save(measure);
     }
 
 }
