11848d3900ffeae109a4abd03727667c346531c9
sescotti <sebastian.scotti@mercadolibre.com>
2016-10-26 03:32:37 -0300

    H2 solo para dev

---
+++ vitalis-web/src/main/java/com/rocket/vitalis/config/H2SivoriPopulator.java
@@ -0,0 +1,36 @@
+package com.rocket.vitalis.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by sscotti on 10/26/16.
+ */
+@Configuration
+@Profile("dev")
+public class H2SivoriPopulator {
+
+    @Autowired
+    private AbstractApplicationContext context;
+
+    @PostConstruct
+    @Profile("dev")
+    public void startDBManager() {
+        EmbeddedH2Console.start();
+        context.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
+            @Override
+            public void onApplicationEvent(ContextClosedEvent event) {
+                EmbeddedH2Console.stop();
+            }
+        });
+    }
+
+}
